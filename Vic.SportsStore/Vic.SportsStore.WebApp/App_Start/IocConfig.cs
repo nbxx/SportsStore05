5a341493aa8ac9e14cfc2feca142fefb2210a0d0
GY <GY>
2020-03-07 15:30:03 -0500

    1 [CHG] config ioc;

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vic.SportsStore.WebApp
+{
+    public class IocConfig
+    {
+        public static void ConfigIoc()
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterControllers(typeof(MvcApplication).Assembly)
+                .PropertiesAutowired();
+
+
+
+
+
+            var container = builder.Build();
+            DependencyResolver
+                .SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
