8759dcf1d2a3b957dd870f711ed316750ac6325d
GY <GY>
2020-04-04 15:26:01 -0400

    1 [CHG] add responsive menu;

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/NavController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/NavController.cs
@@ -19,14 +19,12 @@ namespace Vic.SportsStore.WebApp.Controllers
         public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategory = category;
-
             IEnumerable<string> categories = repository
                 .Products
                 .Select(x => x.Category)
                 .Distinct()
                 .OrderBy(x => x);
-
-            return PartialView(categories);
+            return PartialView("FlexMenu", categories);
         }
 
     }
